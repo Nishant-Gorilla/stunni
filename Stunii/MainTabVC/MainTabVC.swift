ba9c5f4ed5c493f89d30f85e630f8d45e0962c4e
nishant <nb.gorilladevelopment@gmail.com>
2019-10-12 15:27:15 +0530

    load all Update code

---
+++ Stunii/MainTabVC/MainTabVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabVC.swift
+//  Stunii
+//
+//  Created by Harsh Rajput on 27/09/19.
+//  Copyright © 2019 Gorilla App Development. All rights reserved.
+//
+
+import UIKit
+
+class MainTabVC: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Stunii/SearchDeals/SearchDealVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchDealVC.swift
+//  Stunii
+//
+//  Created by Harsh Rajput on 26/09/19.
+//  Copyright © 2019 Gorilla App Development. All rights reserved.
+//
+
+import UIKit
+
+class SearchDealVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
